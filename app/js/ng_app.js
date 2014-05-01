9cd6f38e9a58b6b5ff0ea191cddd2f209bff8d51
christothes <chriscott@hotmail.com>
2014-04-30 22:29:26 -0500

    home page text and navBar

--- app/js/ng_app.js
+++ app/js/ng_app.js
@@ -9,8 +9,9 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'HomeCtrl'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
